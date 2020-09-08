648afa4f6928d5c2ce408fed657e91d301de5eb0
Mathis Koroglu <mathis@koroglu.fr>
2020-09-08 19:39:36 +0200

    Use of esp-idf log system

--- src/debug.h
+++ src/debug.h
@@ -4,29 +4,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#ifdef ESP_IDF
+#include <esp_log.h>
+static const char* HK_TAG = "HomeKit";
+#endif
+
 typedef unsigned char byte;
 
 #ifdef HOMEKIT_DEBUG
-
+#ifdef ESP_IDF
+#define DEBUG(message, ...) ESP_LOGD(HK_TAG, "func %s: " message, __func__, ##__VA_ARGS__)
+#else
 #define DEBUG(message, ...) printf(">>> %s: " message "\n", __func__, ##__VA_ARGS__)
-
+#endif
 #else
 
 #define DEBUG(message, ...)
 
 #endif
 
-#define INFO(message, ...) printf(">>> HomeKit: " message "\n", ##__VA_ARGS__)
-#define ERROR(message, ...) printf("!!! HomeKit: " message "\n", ##__VA_ARGS__)
 
 #ifdef ESP_IDF
-
+#define INFO(message, ...) ESP_LOGI(HK_TAG, message, ##__VA_ARGS__)
+#define ERROR(message, ...) ESP_LOGE(HK_TAG, message, ##__VA_ARGS__)
 #define DEBUG_HEAP() DEBUG("Free heap: %d", esp_get_free_heap_size());
 
 #else
-
+#define INFO(message, ...) printf(">>> HomeKit: " message "\n", ##__VA_ARGS__)
+#define ERROR(message, ...) printf("!!! HomeKit: " message "\n", ##__VA_ARGS__)
 #define DEBUG_HEAP() DEBUG("Free heap: %d", xPortGetFreeHeapSize());
-
 #endif
 
 char *binary_to_string(const byte *data, size_t size);
